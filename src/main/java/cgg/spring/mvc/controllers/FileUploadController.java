5bcc1ebb97ae1c29b42163d5e5f727df8b9a108b
Farooq <fmd894717@gmail.com>
2023-08-29 18:52:48 +0530

    commit 29-08-2023

--- src/main/java/cgg/spring/mvc/controllers/FileUploadController.java
+++ src/main/java/cgg/spring/mvc/controllers/FileUploadController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 @Controller
 public class FileUploadController {
@@ -15,7 +15,7 @@ public class FileUploadController {
 	}
 	
 	@RequestMapping(path="/uploadimage", method=RequestMethod.POST)
-	public String fileUpload(@RequestParam("profile") MultipartFile part) {
+	public String fileUpload(@RequestParam("profile") CommonsMultipartFile part) {
 		if(!part.isEmpty()) {
 			return "fileform";
 		}
